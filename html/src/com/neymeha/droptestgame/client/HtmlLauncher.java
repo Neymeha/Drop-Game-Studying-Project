06d0ebb9f4d8ab47dc77c54b9735f625ef922f4d
Herman Stepanov <neymeha@inbox.ru>
2023-04-16 17:27:04 +0300

    Initial commit

---
+++ html/src/com/neymeha/droptestgame/client/HtmlLauncher.java
@@ -0,0 +1,23 @@
+package com.neymeha.droptestgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.neymeha.droptestgame.Drop;
+import com.neymeha.droptestgame.GameScreen;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(800,480,true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
